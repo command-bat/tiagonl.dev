2cbb47ec5d4d742ca8bfccf1800b336d0459e574
command-bat <tiagolaure@gmail.com>
2026-05-20 02:59:18 -0300

    troquei o projeto

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,15 @@
 const projects = [
+    // {
+    //     title: 'Palpites.bat',
+    //     description:
+    //         'Sistema moderno de palpites esportivos.',
+    //     url: 'https://palpites.commandbat.com.br/'
+    // },
     {
-        title: 'Palpites.bat',
+        title: 'Compartilhamento de Arquivo',
         description:
-            'Sistema moderno de palpites esportivos.',
-        url: 'https://palpites.commandbat.com.br/'
+            'Sistema de compartilhamento de arquivos facilitado.',
+        url: 'https://file.commandbat.com.br/'
     },
     {
         title: 'Pixels Place',
